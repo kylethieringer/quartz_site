cb1c660ff4957f056ddabbce051f891b091a4e79
kylethieringer <kylethieringer@gmail.com>
2024-07-16 18:47:22 -0700

    Quartz sync: Jul 16, 2024, 6:47 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,7 +6,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterbody: [],
+  // afterbody: [],
   footer: Component.Footer(),
 }
 
--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -14,7 +14,7 @@ interface RenderComponents {
   header: QuartzComponent[]
   beforeBody: QuartzComponent[]
   pageBody: QuartzComponent
-  afterBody: QuartzComponent[]
+  // afterBody: QuartzComponent[]
   left: QuartzComponent[]
   right: QuartzComponent[]
   footer: QuartzComponent
@@ -188,7 +188,7 @@ export function renderPage(
     header,
     beforeBody,
     pageBody: Content,
-    afterBody,
+    // afterBody,
     left,
     right,
     footer: Footer,
@@ -234,12 +234,6 @@ export function renderPage(
                 </div>
               </div>
               <Content {...componentData} />
-              <hr />
-              <div class="page-footer">
-                {afterBody.map((BodyComponent) => (
-                  <BodyComponent {...componentData} />
-                ))}
-              </div>
             </div>
             {RightComponent}
           </Body>
